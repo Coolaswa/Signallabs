9ce66d17b661c76b345d3b3576b3a2ed47bfb2f9
Coolaswa <mfavier@live.nl>
2017-09-08 12:25:57 +0200

    Finished assignment 3

---
+++ Lab1_3.m
@@ -0,0 +1,19 @@
+%a) L should equal 5, so that the h[n] equals 1 when n=0,1,2 and equals
+%zero elsewhere.
+
+%b) 
+
+
+x = [1 0 1 0 1 0 1];
+nx = -3:3;
+
+h = [1 1 1];
+nh_causal = 0:2;
+nh = -5:-3;
+
+[ny,y] = convcool(nx, x, nh, h);
+[ny_causal,y_causal] = convcool(nx, x, nh_causal, h);
+
+stem(ny,y);
+hold on;
+stem(ny_causal,y_causal);
